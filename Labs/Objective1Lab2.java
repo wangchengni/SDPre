d229728b9ab01133dca34759d8594f44a3cc3631
larry <wangchengni001@gmail.com>
2021-09-21 13:58:05 -0600

    complete objective1lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,20 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    /*
+      Your solution goes here
+      >>  ----------------
+>> | Grocery List   |
+>> | 1. Milk        |
+>> | 2. Eggs        |
+>> | 3. Bread       |
+>>  ----------------
+
+    */
+    System.out.println("-------------------------");
+    System.out.println("| Grocery List |" );
+    System.out.println("| 1. Milk |");
+    System.out.println("| 2. Eggs |");
+    System.out.println("| 3. Bread |");  
+    System.out.println("-------------------------");
+  }
+}
