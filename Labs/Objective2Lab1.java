58901ec3d7742a195012723f62a7f8cf354897bb
larry <wangchengni001@gmail.com>
2021-09-21 14:30:51 -0600

    complete objective2lab1

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,10 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    String hello = "Hello World";
+
+    /*
+      Your solution goes here
+    */
+    system.out.println(hello);
+  }
+}
