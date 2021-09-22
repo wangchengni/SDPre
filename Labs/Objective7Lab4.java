d81428bd618dce6c4a80022712a06ff844c46352
wangchengni <wangchengni001@gmail.com>
2021-09-22 13:58:48 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args){
+    int count = 0;
+    int sum = 0;
+    while (count <=20){
+      sum = sum + count;
+      count +=1 ;
+    }
+    System.out.println(sum);
+
+  }
+}
