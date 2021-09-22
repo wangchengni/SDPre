c7c2182f0121c8959be48c3dcc6dedf4de43dc32
wangchengni <wangchengni001@gmail.com>
2021-09-22 13:01:17 -0600

    complete objective7lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,14 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    while(counter > 0) {
+
+      /*
+      Your solution goes here
+      */
+      System.out.println(counter);
+      counter = counter - 1; //this makes we count down
+    }
+  }
+}
