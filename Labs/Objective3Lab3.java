aef2558924fef0743493d0037062fb4ca9c164b4
wangchengni <wangchengni001@gmail.com>
2021-09-22 09:03:14 -0600

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,9 @@
+public class Objective3Lab3 {
+  public static void main(String[] args){
+    int first = 1+ (int)(Math.random()*6);
+    int second = 1+ (int)(Math.random()*6);
+    int third = 1+ (int)(Math.random()*6);
+    int sum1 = first +second +third;
+    System.out.println(sum1);
+  }
+}
