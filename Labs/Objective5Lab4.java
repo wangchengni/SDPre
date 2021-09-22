6be9f4b185b87119e25dcf24437282cc2132cbff
wangchengni <wangchengni001@gmail.com>
2021-09-22 12:16:19 -0600

    complete objective5lab4

---
+++ Labs/Objective5Lab4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Objective5Lab4 {
+    public static void main(String[] args) {
+      Scanner kb = new Scanner(System.in);
+      System.out.println("Please enter a number: ");
+      int userNum = kb.nextInt();
+      if (userNum % 2 == 0){
+      System.out.println("The number is even");
+      }
+      else {
+        System.out.println("The number is odd");
+      }
+
+      kb.close();
+
+
+
+  }
+}
