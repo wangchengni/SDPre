0452514985571cd49546a8427c7c87a0ff9cfb99
wangchengni <wangchengni001@gmail.com>
2021-09-21 22:43:47 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    /*double num1, num2, num3, average;*/
+    double num1 = 15.0;
+    double num2 = 6.0;
+    double num3 = 2.0;
+    double average = (num1 +num2 +num3)/3;
+    /*
+      Your solution goes here
+    */
+    System.out.println(average);
+  }
+}
