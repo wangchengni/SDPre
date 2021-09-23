ec283204d12652e15325adaa7df564385140c09b
wangchengni <wangchengni001@gmail.com>
2021-09-23 13:08:59 -0600

    complete objective9lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,14 @@
+public class Objective9Lab1 {
+  public static void main(String[] args){
+    printName();
+
+  }
+    /*
+    Your solution goes here
+    */
+
+
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
