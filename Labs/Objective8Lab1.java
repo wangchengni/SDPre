c3cf3dc0452adb2027dd24e0e85eabfd01bba3f6
wangchengni <wangchengni001@gmail.com>
2021-09-23 12:25:52 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    //TODO Write a for loop that will print the numbers 1-10
+
+    //TODO Write another for loop that will print the numbers 10-1.
+    for(int i =1; i <=10; i++){
+      System.out.println(i);
+    }
+    for (int i =10; i>=1; i--){
+    System.out.println(i);
+  }
+  }
+}
