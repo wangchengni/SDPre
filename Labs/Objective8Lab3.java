5092401c7728904b6e8bb40828e92b7591eb3728
wangchengni <wangchengni001@gmail.com>
2021-09-23 12:48:56 -0600

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,18 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    /*
+      Your solution goes here
+    */
+    for(int i= 1; i<=20; i++){
+      if(i%2 == 0){
+        System.out.println(i+" is even");
+      }
+      else{
+        System.out.println(i +" is odd");
+      }
+    }
+
+  }
+}
